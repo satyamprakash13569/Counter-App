f24a408c859b11f0afdc1276646890ce07afd94b
satyamprakash13569 <121503013+satyamprakash13569@users.noreply.github.com>
2023-08-25 22:37:17 +0530

    Create counterApp.js

---
+++ counterApp.js
@@ -0,0 +1,11 @@
+let countValue=document.getElementById('counter');
+function increment(){
+    let value=parseInt(countValue.innerText);
+    value=value+1;
+    countValue.innerText=value;
+}
+function decrement(){
+    let value=parseInt(countValue.innerText);
+    value=value-1;
+    countValue.innerText=value;
+}
